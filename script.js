4a532ca3348fed85787d5504fd373eed787d8eb6
Hafiz Muhammad Ahmad <i230714@isb.nu.edu.pk>
2025-07-08 20:26:11 +0500

    time for wait for rendering message increased from 3secs to 9 secs

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ consentForm.addEventListener('submit', function(e){
     setTimeout(function(){
         document.getElementById('upload-text').innerText = `
         Making the sale...`
-    }, 1500)
+    }, 4500)
     
     
     setTimeout(function(){
@@ -44,6 +44,6 @@ consentForm.addEventListener('submit', function(e){
         </div>
     `
     modalCloseBtn.disabled = false
-    }, 3000)
+    }, 9000)
   
 }) 
